fae47b14bdf9f083fe50a677c194b5e17f178de4
ghautham.sam01 <ghautham.sam01@gmail.com>
2023-03-15 04:01:35 -0400

    introduced batch and interactive mode

--- myshell.c
+++ myshell.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-    printf("Hello! Welcome to our shell");
+    
+    if(argc==1){
+    //Interactive mode
+        printf("Hello! Welcome to our shell");
+        interactiveMode();
+    }else if(argc==2){
+    //Batch mode
+        batchMode(argv[1]);
+    }else{
+        printf("\n Too many args");
+    }
+
 
     return 0;
+}
+
+int interactiveMode(){
+    char *line;
+    char **args;
+    while(line !='q'){
+        printf("myshell> ");
+        line
+    }
+}
+
+int batchMode(){
+      
 }
