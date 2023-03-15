2a9de7009c5bd76b09f3101d7c1c43790fc64da5
ghautham.sam01 <ghautham.sam01@gmail.com>
2023-03-15 02:50:15 -0400

    init myshell.c/.h

---
+++ myshell.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    printf("Hello! Welcome to Kevin's and Ghautham's shell\n Please include a file name to read commands or start typing the commands!");
+
+    return 0;
+}
